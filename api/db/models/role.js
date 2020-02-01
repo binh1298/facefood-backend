0135ebd30dd170bd9c65db68889bc44498cf4093
Space Potato <binguyentuananh@gmail.com>
2020-02-01 20:34:06 +0700

    add relations to model

--- api/db/models/role.js
+++ api/db/models/role.js
@@ -1,14 +1,8 @@
 /* jshint indent: 1 */
-<<<<<<< HEAD
-
-module.exports = function(sequelize, DataTypes) {
-	return sequelize.define('role', {
-=======
 var user = require('./user.js');
 
 module.exports = function (sequelize, DataTypes) {
 	var role = sequelize.define('role', {
->>>>>>> 228ece6b27e2c8d8e680a8035910bef61fcc1966
 		roleId: {
 			type: DataTypes.INTEGER,
 			allowNull: false,
@@ -23,9 +17,6 @@ module.exports = function (sequelize, DataTypes) {
 	}, {
 		tableName: 'role'
 	});
-<<<<<<< HEAD
-=======
 	role.hasMany(user, { as: 'user' });
 	return role;
->>>>>>> 228ece6b27e2c8d8e680a8035910bef61fcc1966
 };
