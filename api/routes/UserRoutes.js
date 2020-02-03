f8706cbc00fc52dbd2d5119541421b656f5dc153
Binh Pham <binh.pham@nfq.asia>
2020-02-03 11:33:07 +0700

    init routes

---
+++ api/routes/UserRoutes.js
@@ -0,0 +1,16 @@
+  
+'use strict';
+
+/**
+* User Route
+* path: /user
+*/
+
+let express    = require('express');
+let Controller = require('../controllers/UserController');
+let router     = express.Router();
+
+router.get('/', Controller.index.get);
+router.post('/new', Controller.new.post);
+
+module.exports = router;
