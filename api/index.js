d00d4de09c1fb8cdd550cf9e646a3e693848728a
Binh Pham <binh.pham@nfq.asia>
2020-02-03 13:30:47 +0700

    Fix 404 page

--- api/index.js
+++ api/index.js
@@ -1,32 +1,34 @@
 import express from 'express';
-import bodyParser from 'body-parser'; 
+import bodyParser from 'body-parser';
 import Router from './routes';
+const status = require('http-status');
 
 require('dotenv').config();
 
-const app = express(); 
-app.use(bodyParser.json()); 
-app.use(bodyParser.urlencoded({ extended: false })); 
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.status(200).send({
   message: 'Welcome to this API.'
-})); 
+}));
 
 Router.forEach(route => {
   app.use(route.path, route.handler);
 });
 
 app.use((req, res, next) => {
-  res.status(404);
-  res.render('global/404', {
-    title: 'Page not found!',
-  });
+  return res.status(status.NOT_FOUND)
+    .send({
+      success: false,
+      error: 'Page not found!',
+    });
 });
 
 app.listen(port, () => {
   console.log(`Server is running on PORT ${port}`);
-}); 
+});
 
 export default app;
