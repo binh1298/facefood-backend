a5448cc9246c1267ab16095fb53832bbc715322e
Binh Pham <binh1298@gmail.com>
2020-02-08 04:30:25 +0700

    Fix database connection

--- api/db/config/config.js
+++ api/db/config/config.js
@@ -1,9 +1,12 @@
+require('dotenv').config();
+
 module.exports = {
   development: {
-    database: 'facefood_database',
-    username: 'username',
-    password: 'password',
-    host: '127.0.0.1',
+    database: process.env.POSTGRES_DB,
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
     dialect: 'postgres'
   },
 
--- api/db/models/index.js
+++ api/db/models/index.js
@@ -7,47 +7,38 @@ const env = process.env.NODE_ENV ? process.env.NODE_ENV : 'development';
 
 const config = configJson[env];
 
-console.log('this is the environment: ', env);
-
 const db = {};
 
-let sequelize;
-if (config.environment === 'production') {
-  sequelize = new Sequelize(
-      process.env[config.use_env_variable], config
-    );
-  sequelize = new Sequelize(
-    process.env.POSTGRES_DB,
-    process.env.POSTGRES_USER,
-    process.env.POSTGRES_PASSWORD, {
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
-      dialect: 'postgres',
-      dialectOption: {
-        ssl: true,
-        native: true
-      },
-      logging: true
-    }
-  );
-} else {
-  sequelize = new Sequelize(
-     config.database, config.username, config.password, config
-  );
-}
+const sequelize = new Sequelize(config.database, config.username, config.password, {
+  host: config.host,
+  port: config.port,
+  dialect: 'postgres',
+  dialectOption: {
+    ssl: true,
+    native: true
+  },
+  logging: console.log
+});
 
-fs
-  .readdirSync(__dirname)
-  .filter((file) => {
-    return (file.indexOf('.') !== 0) && 
-           (file !== basename) && (file.slice(-3) === '.js');
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
   })
-  .forEach((file) => {
+  .catch(error => {
+    console.error('Unable to connect to the database:', error.message);
+  });
+
+fs.readdirSync(__dirname)
+  .filter(file => {
+    return file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js';
+  })
+  .forEach(file => {
     const model = sequelize.import(path.join(__dirname, file));
     db[model.name] = model;
   });
 
-Object.keys(db).forEach((modelName) => {
+Object.keys(db).forEach(modelName => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
