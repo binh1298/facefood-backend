474434eb3d480258b68576d7db41021b030ff724
Space Potato <binguyentuananh@gmail.com>
2020-02-08 05:44:01 +0700

    update admin routing

--- api/routes/AdminRoutes.js
+++ api/routes/AdminRoutes.js
@@ -9,7 +9,7 @@ let express = require('express');
 let Controller = require('../controllers/AdminController');
 let router = express.Router();
 
-// router.get('/view-users', Controller.viewUsers);
-// router.get('/view-posts', Controller.viewPosts);
+router.get('/view-users', Controller.viewUsers.get);
+router.get('/view-posts', Controller.viewPosts.get);
 
 module.exports = router;
