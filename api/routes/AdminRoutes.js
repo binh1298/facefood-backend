3594e7c79399eecf077bb5190e4c0acd1bdc7a8e
Space Potato <binguyentuananh@gmail.com>
2020-02-08 07:16:10 +0700

    CRUD for posts & users

--- api/routes/AdminRoutes.js
+++ api/routes/AdminRoutes.js
@@ -10,8 +10,11 @@ let UserController = require('../controllers/UserController');
 let PostController = require('../controllers/PostController');
 let router = express.Router();
 //user
-router.get('/users/view', UserController.index.get);
+router.get('/users/view-all', UserController.index.get);
 router.post('/users/add',UserController.new.post);
 //post
+router.get('/posts/view-all',PostController.index.get)
 router.post('/posts/create',PostController.create.post)
+router.post('/posts/delete',PostController.delete.post)
+router.get('/posts/view-one',PostController.view.get)
 module.exports = router;
