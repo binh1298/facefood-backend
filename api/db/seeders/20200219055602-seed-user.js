b919565e8e378e059da54746752e7acf16234e81
SpacePotato <anh.nguyen0909@knorex.com>
2020-02-20 11:06:47 +0700

    update user seed

--- api/db/seeders/20200219055602-seed-user.js
+++ api/db/seeders/20200219055602-seed-user.js
@@ -5,6 +5,7 @@ module.exports = {
     return queryInterface.bulkInsert('user', [{
       user_id: '468ab892-7518-4520-8243-db1c1b9607dd',
       username: 'SpacePotato',
+      fullname: 'Hello Space Potato',
       password: 'potatopassword',
       email: 'space@potato.com',
       created_at: new Date(),
@@ -14,6 +15,7 @@ module.exports = {
       {
         user_id: 'bfad3537-875c-4bf2-bb97-41c00b912d76',
         username: 'Potato',
+        fullname: 'Hello Potato',
         password: 'potatopassword',
         email: 'potato@potato.com',
         created_at: new Date(),
@@ -23,6 +25,7 @@ module.exports = {
       {
         user_id: '9903c282-06ed-48fe-9607-76e7903f6b72',
         username: 'BinhPham',
+        fullname: 'Pham Duc Binh',
         password: 'binhphampassword',
         email: 'binh@pham.com',
         created_at: new Date(),
@@ -32,6 +35,7 @@ module.exports = {
       {
         user_id: 'ef71e125-37b5-4a5f-87e1-fdda43a4ccb2',
         username: 'DucPhi',
+        fullname: 'Phi Do Hong Duc',
         password: 'ducphipassword',
         email: 'duc@phi.com',
         created_at: new Date(),
@@ -41,6 +45,7 @@ module.exports = {
       {
         user_id: 'da9c7b32-0f05-48f7-b74d-d052df2347d2',
         username: 'LocTrinh',
+        fullname: 'Trinh Thien Loc',
         password: 'loctrinhpassword',
         email: 'loc@trinh.com',
         created_at: new Date(),
