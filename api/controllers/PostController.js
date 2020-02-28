5bca09d5db9ee3634f6b669b8614a101397204d0
SpacePotato <anh.nguyen0909@knorex.com>
2020-02-28 15:51:17 +0700

    update post response

--- api/controllers/PostController.js
+++ api/controllers/PostController.js
@@ -3,6 +3,7 @@ const models = require('../db/models/index');
 const status = require('http-status');
 const {Op} = require("sequelize");
 const url = require('url');
+const sequelize = require('sequelize');
 
 module.exports = {
   view: {
@@ -18,25 +19,37 @@ module.exports = {
         const orderOptions = queryData.order.split(",");
         const posts = await models.Post
           .findAll({
-            attributes: [
-              'postId',
-              'postName',
-              'description',
-              'timeNeeded',
-              'userId',
-              'categoryId',
-              'isDeleted',
-              'createdAt',
-              'updatedAt',
+            attributes: {
+              include: [
+                [sequelize.fn('COUNT', sequelize.col('Likes.like_id')), 'totalLikes'],
+                [sequelize.fn('COUNT', sequelize.col('Comments.comment_id')), 'totalComments'],
+                [sequelize.fn('COUNT', sequelize.col('Steps.step_id')), 'totalSteps'],
+              ],
+              exclude: ['category_id', 'user_id', 'userId']
+            },
+            include: [
+              {
+                model: models.Like,
+                attributes: [],
+              },
+              {
+                model: models.Comment,
+                attributes: [],
+              },
+              {
+                model: models.Step,
+                attributes: [],
+              }
             ],
             where: {
               post_name: {
                 [Op.iLike]: '%' + queryData.postName + '%'
               }
-            },
+            }, group: ['Post.post_id', 'Likes.like_id', 'Comments.comment_id', 'Steps.step_id'],
             order: [
               [orderOptions[0], orderOptions[1]],
-            ]
+            ],
+            raw: false,
           });
         res.status(status.OK)
           .send({
--- api/db/seeders/20200219055953-seed-post.js
+++ api/db/seeders/20200219055953-seed-post.js
@@ -12,7 +12,8 @@ module.exports = {
         created_at: new Date(),
         updated_at: new Date(),
         category_id: '814c39e1-c76a-4b0c-9d1e-714d601cdcff',
-        user_id: '468ab892-7518-4520-8243-db1c1b9607dd'
+        user_id: '468ab892-7518-4520-8243-db1c1b9607dd',
+        username: 'SpacePotato',
       },
       {
         post_id: '36d0dca0-994f-4dac-b71d-3a9189b84758',
@@ -23,7 +24,8 @@ module.exports = {
         created_at: new Date(),
         updated_at: new Date(),
         category_id: '472a49b0-8390-4718-a5ec-ea04629b31a1',
-        user_id: '468ab892-7518-4520-8243-db1c1b9607dd'
+        user_id: '468ab892-7518-4520-8243-db1c1b9607dd',
+        username: 'SpacePotato',
       },
       {
         post_id: '8f8a506b-cb8c-4d70-bda8-b86dd88dd7d7',
@@ -34,7 +36,8 @@ module.exports = {
         created_at: new Date(),
         updated_at: new Date(),
         category_id: '472a49b0-8390-4718-a5ec-ea04629b31a1',
-        user_id: 'ef71e125-37b5-4a5f-87e1-fdda43a4ccb2'
+        user_id: 'ef71e125-37b5-4a5f-87e1-fdda43a4ccb2',
+        username: 'DucPhi',
       },
       {
         post_id: 'b8c24881-e053-46f1-afea-6b27653905d6',
@@ -45,7 +48,8 @@ module.exports = {
         created_at: new Date(),
         updated_at: new Date(),
         category_id: '472a49b0-8390-4718-a5ec-ea04629b31a1',
-        user_id: 'ef71e125-37b5-4a5f-87e1-fdda43a4ccb2'
+        user_id: 'ef71e125-37b5-4a5f-87e1-fdda43a4ccb2',
+        username: 'DucPhi',
       },
       {
         post_id: 'bb9ace81-51ff-4941-84dd-2922404b970d',
@@ -56,7 +60,8 @@ module.exports = {
         created_at: new Date(),
         updated_at: new Date(),
         category_id: '472a49b0-8390-4718-a5ec-ea04629b31a1',
-        user_id: 'ef71e125-37b5-4a5f-87e1-fdda43a4ccb2'
+        user_id: 'ef71e125-37b5-4a5f-87e1-fdda43a4ccb2',
+        username: 'DucPhi',
       },
       {
         post_id: '0cab3544-1956-4c0e-ac2a-924fcca259a3',
@@ -67,7 +72,8 @@ module.exports = {
         created_at: new Date(),
         updated_at: new Date(),
         category_id: '472a49b0-8390-4718-a5ec-ea04629b31a1',
-        user_id: 'ef71e125-37b5-4a5f-87e1-fdda43a4ccb2'
+        user_id: 'ef71e125-37b5-4a5f-87e1-fdda43a4ccb2',
+        username: 'DucPhi',
       },
       {
         post_id: '46c76de2-bc37-4feb-918b-06a7a76f81b0',
@@ -78,9 +84,10 @@ module.exports = {
         created_at: new Date(),
         updated_at: new Date(),
         category_id: '472a49b0-8390-4718-a5ec-ea04629b31a1',
-        user_id: 'ef71e125-37b5-4a5f-87e1-fdda43a4ccb2'
+        user_id: 'ef71e125-37b5-4a5f-87e1-fdda43a4ccb2',
+        username: 'DucPhi',
       },
-      ], {});
+    ], {});
   },
 
   down: (queryInterface, Sequelize) => {
