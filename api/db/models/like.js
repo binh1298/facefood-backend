2d7a514f29c4b467989c9fb13927cfdf670a369b
Space Potato <binguyentuananh@gmail.com>
2020-03-20 09:00:53 +0700

    [add] -> Check Like status

--- api/db/models/like.js
+++ api/db/models/like.js
@@ -4,12 +4,10 @@ module.exports = function (sequelize, DataTypes) {
   return sequelize.define('Like', {
     userId: {
       type: DataTypes.UUID,
-      allowNull: false,
       field: 'user_id'
     },
     username: {
       type: DataTypes.STRING,
-      allowNull: false,
       field: 'username'
     },
     postId: {
