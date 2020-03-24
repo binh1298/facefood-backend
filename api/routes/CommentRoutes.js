defa40bfec62f193a3a3d020938e380e0a756101
Space Potato <binguyentuananh@gmail.com>
2020-03-24 17:18:58 +0700

    [update] fix routing

--- api/routes/CommentRoutes.js
+++ api/routes/CommentRoutes.js
@@ -9,7 +9,7 @@ let express = require('express');
 let Controller = require('../controllers/CommentController');
 let router = express.Router();
 
-router.get('/', Controller.view_by_post_id.get);
+router.get('/', Controller.view.get);
 router.get('/:postId', Controller.view_by_post_id.get);
 router.post('/', Controller.create.post);
 router.put('/:commentId/delete', Controller.delete.put);
